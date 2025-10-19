54c9ff175c14b8d07f383f4cf9b114500dd7e892
Ishrak826 <amk1011334@student.hamk.fi>
2025-10-19 22:53:27 +0300

    2nd step
    Then I printed the app name,the creator name also I printed the numbers

--- githubproject/src/App.java
+++ githubproject/src/App.java
@@ -4,5 +4,10 @@ public class App {
       int number1 = 5;
       int number2 = 2;
       //System.out.println(number1);
-      int multiplication,addition,subtraction;
-      double division;}}
+      int multiplication,addition,substraction;
+      double division;
+      System.out.println("Printer App");
+      System.out.println("This app is made by: " + creator);
+      System.out.println("The value of number1 is " + number1);
+      System.out.println("The value of number2 is " + number2);
+}}
