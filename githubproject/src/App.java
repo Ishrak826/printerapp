9781e92d888177a36733a7f20ea66147a8280ac6
Ishrak826 <amk1011334@student.hamk.fi>
2025-10-19 22:59:39 +0300

    3rd step
    in this step I had done the addition, subtraction, multiplication and division part. In the division part I pointed the numbers as double so that I can convert the int numbers into double for division purpose.
    The Printer App is complete.

--- githubproject/src/App.java
+++ githubproject/src/App.java
@@ -4,10 +4,18 @@ public class App {
       int number1 = 5;
       int number2 = 2;
       //System.out.println(number1);
-      int multiplication,addition,substraction;
+      int multiplication,addition,subtraction;
       double division;
       System.out.println("Printer App");
       System.out.println("This app is made by: " + creator);
       System.out.println("The value of number1 is " + number1);
       System.out.println("The value of number2 is " + number2);
-}}
+        multiplication = number1 * number2;
+      System.out.println(number1+" * "+number2+" = "+multiplication);
+      addition=number1+number2;
+      System.out.println(number1+" + "+number2+" = "+addition);
+      division=(double)number1/number2;
+      System.out.println(number1+" / "+number2+" = "+division);
+      subtraction=number1-number2;
+      System.out.println(number1+" - "+number2+" = "+ subtraction);
+        }}
